f3c3d6a12fba2357d6615537747eb8c5ec7c4008
Andrew Meier <andrewmeier@andrews-mbp.lan>
2024-06-24 23:34:03 -0400

    Created env file

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -6,9 +6,11 @@ import Link from "next/link";
 
 
 export default function Contact() {
-
+    const captchaKey: string = (process.env.NEXT_PUBLIC_CAPTCA_SECRET as string);
     const recaptcha = React.useRef<ReCAPTCHA>(null);
     const [loading, setLoading] = useState(false);
+
+    console.log(`Site key${captchaKey}`)
 
     async function onChange(value: any) {
         // verify captcha
@@ -46,7 +48,7 @@ export default function Contact() {
                             <input type="text" className="w-3/4 sm:w-1/2 lg:w-1/3 px-3 py-2 outline-none drop-shadow-xl rounded-lg h-[51px] z-0" placeholder="Enter your email" />
                             <ReCAPTCHA
                                 ref={recaptcha}
-                                sitekey={'6LfMdfgpAAAAAMKSHH_c7zjHwBl3DZ4YAQQwW2yp'}
+                                sitekey={captchaKey}
                                 onChange={onChange}
                             />
                             <button type="button" className="px-3 py-2 bg-button-primary-disabled text-slate-800 rounded-md hover:bg-button-primary-hover">
