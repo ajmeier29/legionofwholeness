93db21406a25b785efc2455233125e4644086e8c
Andrew Meier <andrewmeier@andrews-mbp.lan>
2024-07-05 01:51:32 -0400

    Modified PreloadImage

--- src/components/PreloadImage.tsx
+++ src/components/PreloadImage.tsx
@@ -10,7 +10,7 @@ export default function PreloadImage(props: Props) {
     return (
         <>
             <Head>
-                <link rel="preload" href={props?.imgSrc || ''} as="image" />
+                <link rel="preload" fetchPriority="high" as="image" href={props?.imgSrc || ''} type="image/webp"></link>
             </Head>
             <img className={props?.styleProps || ''} src={props?.imgSrc || ''} />
         </>
