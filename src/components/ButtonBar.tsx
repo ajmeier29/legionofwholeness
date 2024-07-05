2b9162137db7f1ad65a5e478d4e02f87511de632
Andrew Meier <andrewmeier@andrews-mbp.lan>
2024-07-04 22:27:36 -0400

    Added blog page

--- src/components/ButtonBar.tsx
+++ src/components/ButtonBar.tsx
@@ -7,7 +7,7 @@ export default function ButtonBar() {
         <>
             <div className="grid grid-cols-1 md:grid-cols-3 gap-4 mt-3">
                 <ButtonBarText title={'Schedule an appointment'} description={'Schedule an appointment today to start to remember your true self.'} />
-                <Link href={'/BlogPost'}>
+                <Link href={'/BlogPosts'}>
                     <ButtonBarText title={'Read the latest Blogs'} description={'Find inspiration by reading our latest blogs. '} />
                 </Link>
                 <ButtonBarText title={'Subscribe to our Newsletter'} description={'Subscribe to my newsletter for weekly inspiration.'} />
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,6 +1,11 @@
 import GradiantText from './GradientText';
 
-export default function Hero() {
+
+type HeroProps =
+    {
+        subscribe: boolean
+    }
+export default function Hero(props: HeroProps) {
     return (
         <>
             <div className="absolute bottom-6 md:bottom-20 left-7 md:left-12">
@@ -9,11 +14,13 @@ export default function Hero() {
                     <h2 className='bg-hero-gradient text-[40px] md:text-[55px] font-extrabold inline text-transparent bg-clip-text'>WHOLENESS</h2>
 
                     <p className='text-primary text-[14px] md:text-[17px]'>Helping others to find their strength through remembering their true self.</p>
-
-                    <button type="button" className="mt-5 px-3 py-3 bg-button-primary text-slate-800 rounded-md hover:bg-button-primary-hover relative items-end">
-                        Subscribe
-                        <svg className="inline h-5 w-5 text-slate-600" width="24" height="24" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">  <path stroke="none" d="M0 0h24v24H0z" />  <polyline points="9 6 15 12 9 18" /></svg>
-                    </button>
+                    {props.subscribe ?
+                        (<button type="button" className="mt-5 px-3 py-3 bg-button-primary text-slate-800 rounded-md hover:bg-button-primary-hover relative items-end">
+                            Subscribe
+                            <svg className="inline h-5 w-5 text-slate-600" width="24" height="24" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">  <path stroke="none" d="M0 0h24v24H0z" />  <polyline points="9 6 15 12 9 18" /></svg>
+                        </button>)
+                        : (<></>)
+                    }
                 </div>
             </div>
         </>
