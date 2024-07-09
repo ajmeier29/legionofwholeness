4929a34c9dd8e26ecead110479f015995769cc72
Andrew Meier <andrewmeier@andrews-mbp.lan>
2024-07-09 00:00:54 -0400

    Fixed toast message display and made subscribe fixed

--- src/components/ToastMessage.tsx
+++ src/components/ToastMessage.tsx
@@ -9,7 +9,7 @@ type ToastProps =
 const ToastMessage = ({ message }: ToastProps) => {
     return (
         <>
-            <div className={`absolute bg-rust py-2 px-4 rounded-md text-white text-center top-4 right-4 flex gap-4 z-50 toast show`}>
+            <div className={`fixed flex bg-rust py-2 px-4 rounded-md text-white text-center top-4 right-4 gap-4 z-50 toast show`}>
                 <p>{message}</p>
             </div>
         </>
