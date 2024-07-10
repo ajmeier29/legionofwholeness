d4e9154e7592155d0fcd548180253eed4ba7f76a
Andrew Meier <andrewmeier@andrews-mbp.lan>
2024-07-09 22:35:14 -0400

    Fixed warning messages

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -50,7 +50,7 @@ export default function Footer() {
                             <a href='https://www.facebook.com/jbredbenner?mibextid=ZbWKwL'
                                 aria-label='Link to Facebook Page'
                                 className="border border-rust p-2 rounded-full aspect-square text-rust transition-all duration-500 hover:text-slate-600 hover:border-slate-600 focus-within:outline-0 focus-within:text-slate-600 focus-within:border-slate-600">
-                                <svg width="20" height="20" className="" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">  <path d="M18 2h-3a5 5 0 0 0-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 0 1 1-1h3z" /></svg>
+                                <svg width="20" height="20" className="" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">  <path d="M18 2h-3a5 5 0 0 0-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 0 1 1-1h3z" /></svg>
                             </a>
                         </div>
                     </div>
