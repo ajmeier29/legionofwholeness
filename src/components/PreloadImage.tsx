7d467eee21d828a66cc055325c7ab3f5ce6e543c
Andrew Meier <andrewmeier@andrews-mbp.lan>
2024-07-12 19:09:31 -0400

    Code cleanup

--- src/components/PreloadImage.tsx
+++ src/components/PreloadImage.tsx
@@ -12,10 +12,6 @@ export default function PreloadImage(props: Props) {
     var styles = `relative ${props?.styleProps}`;
     return (
         <>
-            {/* <Head>
-                <link rel="preload" fetchPriority="high" as="image" href={props?.imgSrc || ''} type="image/webp"></link>
-            </Head> */}
-            {/* <img className={props?.styleProps || ''} src={props?.imgSrc || ''} /> */}
             <div className={styles}>
                 <Image
                     alt={props?.alt ?? ''}
@@ -39,10 +35,6 @@ const PreloadStaticImage = (props: StaticProps) => {
     var styles = `relative ${props?.styleProps}`;
     return (
         <>
-            {/* <Head>
-                <link rel="preload" fetchPriority="high" as="image" href={props?.imgSrc || ''} type="image/webp"></link>
-            </Head> */}
-            {/* <img className={props?.styleProps || ''} src={props?.imgSrc || ''} /> */}
             <div className={styles}>
                 <Image
                     alt=''
