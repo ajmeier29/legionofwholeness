2fbd5d4d892bee0f8c3fba01f9b86488261bd378
Andrew Meier <andrewmeier@andrews-mbp.lan>
2024-07-12 16:41:34 -0400

    Switched to using next.js Image

--- src/components/PreloadImage.tsx
+++ src/components/PreloadImage.tsx
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import Image from "next/image";
 
 type Props =
     {
@@ -7,12 +8,21 @@ type Props =
     }
 
 export default function PreloadImage(props: Props) {
+    var styles = `relative ${props?.styleProps}`;
     return (
         <>
-            <Head>
+            {/* <Head>
                 <link rel="preload" fetchPriority="high" as="image" href={props?.imgSrc || ''} type="image/webp"></link>
-            </Head>
-            <img className={props?.styleProps || ''} src={props?.imgSrc || ''} />
+            </Head> */}
+            {/* <img className={props?.styleProps || ''} src={props?.imgSrc || ''} /> */}
+            <div className={styles}>
+                <Image
+                    alt=''
+                    fill
+                    sizes={"(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"}
+                    src={props?.imgSrc || ''}
+                />
+            </div>
         </>
     )
 }
